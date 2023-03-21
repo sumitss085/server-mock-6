fb34a9976ef785451da444902e07464197e91e65
sumitss <sumitshashtrakar@gmail.com>
2023-03-21 11:47:52 +0530

    add new endpoint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+            const middlewares = jsonServer.defaults();
 const port = 8080; // you can use any port number here
 
 server.use(middlewares);
-server.use(router);
+                 server.use(router);
 server.listen(port);
